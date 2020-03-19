f9039dfc114c0d941c2bd8a596c42864230fb921
Naoki Nishida <domitry@gmail.com>
2020-03-20 01:42:40 +0900

    refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -9,101 +9,103 @@ import Linear.Vector
 import Linear.V3
 import Linear.Metric
 
-type Color = (Float, Float, Float)
+type Color = V3 Float
 type Vf = V3 Float
 data Image = Image Int Int [Color] deriving Show
 data Ray = Ray Vf Vf deriving Show -- orig, dir
 data Camera = Camera Vf Vf Vf Vf deriving Show -- orig, lower left corner, hor, vert
 data HitEvent = HitEvent Float Vf Vf deriving Show -- param, point, normal
 data Pixel = Pixel Float Float Float Float
-
--- solution from https://stackoverflow.com/questions/7787317/list-of-different-types
-data Hittable = Hittable{ hit::Ray->Maybe HitEvent }
+data Hittable = Hittable{ hit::Ray->Maybe HitEvent } --, event::Maybe Float->Maybe HitEvent }
 data World = World [Hittable]
 
-hitSphere::Ray->Vf->Float->Maybe Float
-hitSphere ray vc r
-    | t < 0 = Nothing
-    | (-b-sqrt(t))/2.0/a < 0 = Nothing -- a hitted point is back to origin 
-    | otherwise = Just ((-b-sqrt(t))/2.0/a) -- select a point which is the nearest to the origin of the ray
-    where
-        Ray va vb = ray
-        a = dot vb vb
-        b = 2*dot vb (va-vc)
-        c = dot (va-vc) (va-vc) - r*r
-        t = b*b-4*a*c
-
-sphere::Vf->Float->Hittable
-sphere center rad = Hittable { hit = hit_}
-    where
-        hit_ ray = case (hitSphere ray center rad) of
-            Nothing -> Nothing
-            Just param -> Just $ HitEvent param point normal
-                where
-                    point = extend ray param
-                    normal = point - center
-
-hitWorld::Ray->World->Maybe HitEvent
-hitWorld ray (World hittables) = case (events) of
-    [] -> Nothing
-    _ -> Just $ minimumBy (comparing (\(HitEvent param _ _)->param)) events
-    where
-        events = catMaybes [hit_(ray) | (Hittable hit_) <- hittables]
+getParam::HitEvent->Float
+getParam (HitEvent param _ _) = param
 
 extend::Ray->Float->Vf
 extend (Ray orig dir) t = orig + t*^dir
 
-ray::Float->Float->Camera->Ray
-ray u v cam = Ray orig dir
+positiveOrNothing::Float->Maybe Float
+positiveOrNothing n = if n >= 0 then Just n else Nothing
+
+minimumByMaybe::(a -> a -> Ordering) -> [a] -> Maybe a
+minimumByMaybe lmd lst = if null lst then Nothing else Just $ minimumBy lmd lst
+
+average::[Vf] -> Vf
+average vecs = V3 (rt/nf) (gt/nf) (bt/nf)
+    where
+        (rt,gt,bt,n) = foldl (\(r1,g1,b1,cnt)->
+            (\(V3 r2 g2 b2)->(r1+r2,g1+g2,b1+b2,cnt+1))) (0.0,0.0,0.0,0) vecs
+        nf = fromIntegral n
+
+-- a hitted point is back to origin
+-- select a point which is the nearest to the origin of the ray
+sphere::Vf->Float->Hittable
+sphere vc r = Hittable { hit = hit_ }
+    where
+        hitParam (Ray va vb)
+            | t < 0 = Nothing
+            | otherwise = positiveOrNothing ((-b-sqrt(t))/2.0/a)
+            where
+                a = dot vb vb
+                b = 2*dot vb (va-vc)
+                c = dot (va-vc) (va-vc) - r*r
+                t = b*b-4*a*c
+        
+        hit_ ray = do
+            param <- hitParam ray
+            let point = extend ray param
+            let normal = point - vc
+            return $ HitEvent param point normal
+
+hitWorld::Ray->World->Maybe HitEvent
+hitWorld ray (World hitables) = minimumByMaybe (comparing getParam) events
+    where
+        events = catMaybes $ map (flip hit ray) hitables
+
+genRay::Float->Float->Camera->Ray
+genRay u v cam = Ray orig dir
     where
         Camera orig llcorner hor vert = cam 
         dist = llcorner + (u *^ hor) + (v *^ vert)
         dir = dist - orig
 
 background::Ray->Color
-background ray = (r, g, b)
+background ray = (1.0-t)*^(V3 1.0 1.0 1.0) + t*^(V3 0.5 0.7 1.0)
     where
-        Ray orig (V3 x y z) = ray
+        Ray _ (V3 x y z) = ray
         t = 0.5*(y+1.0) -- bound to [0,1]
-        vec = (1.0-t)*^(V3 1.0 1.0 1.0) + t*^(V3 0.5 0.7 1.0)
-        V3 r g b = vec
 
 color::Ray->World->Color
 color ray world = case (hitWorld ray world) of
     Nothing -> background ray
-    Just (HitEvent _ _ normal) -> let V3 x y z = normalize normal in (0.5*(x+1.0), 0.5*(y+1.0), 0.5*(z+1.0))
-
-mixColors::[Color]->Color
-mixColors cols = (rt/nf, gt/nf, bt/nf)
-    where
-        total = foldl (\(r1,g1,b1,cnt)->
-            (\(r2,g2,b2)->(r1+r2,g1+g2,b1+b2,cnt+1))) (0.0,0.0,0.0,0) cols
-        (rt,gt,bt,n) = total
-        nf = fromIntegral n
-
-colorPixel::Pixel->Int->Camera->World->Color
-colorPixel (Pixel u v uw uh) ns cam world = mixColors cols
-    where
-        rs = take (2*ns) $ randomRs (0.0,1.0) (mkStdGen 300)
-        us = [u + r*uw | r <- take ns rs]
-        vs = [v + r*uh | r <- drop ns rs]
-        cols = map (\(u,v)->color (ray u v cam) world) $ zip us vs
+    Just (HitEvent _ _ normal) -> V3 (0.5*(x+1.0)) (0.5*(y+1.0)) (0.5*(z+1.0))
+        where 
+            V3 x y z = normalize normal
 
 render::Int->Int->Camera->World->Image
 render nx ny cam world = Image nx ny cols
     where
+        ns = 100 -- number of sampling rays
+        gen = mkStdGen 100
+        genColor (x, y) = color (genRay u v cam) world
+            where
+                u = x/(fromIntegral nx)
+                v = 1.0 - y/(fromIntegral ny)
+        sample gen (cx, cy) = [(cx+dx, cy+dy) | (dx, dy) <- zip dxs dys]
+            where
+                (gen0, gen1) = split gen
+                dxs = take ns $ randomRs (0.0, 1.0) gen0
+                dys = take ns $ randomRs (0.0, 1.0) gen1
         xys = [(fromIntegral x, fromIntegral y) | y<-[0..(ny-1)], x<-[0..(nx-1)]]
-        uw = 1.0/(fromIntegral nx)
-        uh = 1.0/(fromIntegral ny)
-        uvs = [(x*uw,1.0-y*uh) | (x, y) <- xys]
-        cols = [colorPixel (Pixel u v uw uh) 100 cam world | (u, v) <- uvs]
-
+        cols = map (\xy -> average $ map genColor (sample gen xy)) xys
+        
 toPPM::Image->String
 toPPM im = unlines(header ++ body)
     where
         Image nx ny cols = im
         header = ["P3",  unwords [show nx, show ny], "255"] -- magic, width, height, maxval
-        body = [unwords $ map (\c -> show $ floor $ 255.9*c) [r,g,b] | (r,g,b) <- cols]
+        body = [unwords $ map (\c -> show $ floor $ 255.9*c) [r,g,b] | (V3 r g b) <- cols]
 
 main = do
     let cam = Camera (V3 0.0 0.0 0.0) (V3 (-2.0) (-1.0) (-1.0)) (V3 4.0 0.0 0.0) (V3 0.0 2.0 0.0)
