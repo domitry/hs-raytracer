abd686e5d3e7e2844fd8f9c72f42377cfeb1e676
Naoki Nishida <domitry@gmail.com>
2020-03-21 16:54:05 +0900

    diffuse

--- app/Main.hs
+++ app/Main.hs
@@ -26,7 +26,7 @@ extend::Ray->Float->Vf
 extend (Ray orig dir) t = orig + t*^dir
 
 positiveOrNothing::Float->Maybe Float
-positiveOrNothing n = if n >= 0 then Just n else Nothing
+positiveOrNothing n = if n > 0.001 then Just n else Nothing
 
 minimumByMaybe::(a -> a -> Ordering) -> [a] -> Maybe a
 minimumByMaybe lmd lst = if null lst then Nothing else Just $ minimumBy lmd lst
@@ -37,6 +37,9 @@ average vecs = V3 (rt/nf) (gt/nf) (bt/nf)
         (rt,gt,bt,n) = foldl (\(r1,g1,b1,cnt)->
             (\(V3 r2 g2 b2)->(r1+r2,g1+g2,b1+b2,cnt+1))) (0.0,0.0,0.0,0) vecs
         nf = fromIntegral n
+
+mkGens::StdGen->[StdGen]
+mkGens gen = unfoldr (\gen-> Just $ split gen) gen
 
 -- a hitted point is back to origin
 -- select a point which is the nearest to the origin of the ray
@@ -55,7 +58,7 @@ sphere vc r = Hittable { hit = hit_ }
         hit_ ray = do
             param <- hitParam ray
             let point = extend ray param
-            let normal = point - vc
+            let normal = (point - vc)^/r
             return $ HitEvent param point normal
 
 hitWorld::Ray->World->Maybe HitEvent
@@ -71,24 +74,41 @@ genRay u v cam = Ray orig dir
         dir = dist - orig
 
 background::Ray->Color
-background ray = (1.0-t)*^(V3 1.0 1.0 1.0) + t*^(V3 0.5 0.7 1.0)
+background (Ray _ dir) = (1.0-t)*^(V3 1.0 1.0 1.0) + t*^(V3 0.5 0.7 1.0)
     where
-        Ray _ (V3 x y z) = ray
+        V3 x y z = normalize dir
         t = 0.5*(y+1.0) -- bound to [0,1]
 
-color::Ray->World->Color
-color ray world = case (hitWorld ray world) of
+randomPointInUnitSphere::StdGen->(Vf, StdGen)
+randomPointInUnitSphere gen0
+    | norm vec <= 1 = (vec, gen3)
+    | otherwise = randomPointInUnitSphere gen3
+    where
+        (x, gen1) = randomR (-1, 1) gen0
+        (y, gen2) = randomR (-1, 1) gen1
+        (z, gen3) = randomR (-1, 1) gen2
+        vec = V3 x y z
+
+reflect::HitEvent->StdGen->(Ray, StdGen)
+reflect (HitEvent _ point normal) gen0 = (Ray point dir, gen1)
+    where
+        (v, gen1) = randomPointInUnitSphere gen0
+        dir = (normalize normal) -- + v
+
+color::Ray->World->StdGen->Color
+color ray world gen0 = case (hitWorld ray world) of
     Nothing -> background ray
-    Just (HitEvent _ _ normal) -> V3 (0.5*(x+1.0)) (0.5*(y+1.0)) (0.5*(z+1.0))
-        where 
-            V3 x y z = normalize normal
+    Just event -> 0.5 *^ color reflected world gen1 --V3 ((x+1)/2) ((y+1)/2) ((z+1)/2) --
+        where
+            (reflected, gen1) = reflect event gen0
+            Ray orig dir = reflected
+            V3 x y z = normalize dir
 
 render::Int->Int->Camera->World->Image
 render nx ny cam world = Image nx ny cols
     where
         ns = 100 -- number of sampling rays
-        gen = mkStdGen 100
-        genColor (x, y) = color (genRay u v cam) world
+        genColor gen (x, y) = color (genRay u v cam) world gen
             where
                 u = x/(fromIntegral nx)
                 v = 1.0 - y/(fromIntegral ny)
@@ -98,7 +118,8 @@ render nx ny cam world = Image nx ny cols
                 dxs = take ns $ randomRs (0.0, 1.0) gen0
                 dys = take ns $ randomRs (0.0, 1.0) gen1
         xys = [(fromIntegral x, fromIntegral y) | y<-[0..(ny-1)], x<-[0..(nx-1)]]
-        cols = map (\xy -> average $ map genColor (sample gen xy)) xys
+        gens = mkGens $ mkStdGen 100
+        cols = map (\(xy, gen) -> average $ map (genColor gen) (sample gen xy)) (zip xys gens)
         
 toPPM::Image->String
 toPPM im = unlines(header ++ body)
@@ -111,5 +132,5 @@ main = do
     let cam = Camera (V3 0.0 0.0 0.0) (V3 (-2.0) (-1.0) (-1.0)) (V3 4.0 0.0 0.0) (V3 0.0 2.0 0.0)
     let small = sphere (V3 0.0 0.0 (-1.0)) 0.5
     let big = sphere (V3 0.0 (-100.5) (-1.0)) 100.0
-    let world = World [big, small] 
-    putStr $ toPPM $ render 800 400 cam world
+    let world = World [small, big] 
+    putStr $ toPPM $ render 200 100 cam world
