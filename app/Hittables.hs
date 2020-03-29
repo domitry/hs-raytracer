0cb099bfee6ae189231d213428bd18a1735ab128
Naoki Nishida <domitry@gmail.com>
2020-03-29 22:03:12 +0900

    fix bug about hit function of sphere

--- app/Hittables.hs
+++ app/Hittables.hs
@@ -5,22 +5,26 @@ module Hittables where
     import Types
     import Utils
 
-    -- a hitted point is back to origin
     -- select a point which is the nearest to the origin of the ray
     sphere::Vf->Float->Material->Hittable
     sphere vc r mat = Hittable { hit = hit_ }
         where
-            hitParam (Ray va vb)
+            hitParam (tmin, tmax) (Ray va vb)
                 | d < 0 = Nothing
-                | otherwise = boundMaybe (1e-3,1e5) ((-b-sqrt(d))/2.0/a)
+                | small > tmin && small < tmax  = Just small
+                | big   > tmin && big   < tmax  = Just big
+                | otherwise = Nothing
                 where
                     a = dot vb vb
                     b = 2*dot vb (va-vc)
                     c = dot (va-vc) (va-vc) - r*r
                     d = b*b-4*a*c
+                    sqd = sqrt d
+                    small = (-b-sqd)/2/a
+                    big = (-b+sqd)/2/a
             
             hit_ ray = do
-                param <- hitParam ray
+                param <- hitParam (1e-3,1e5) ray
                 let point = extend ray param
                 let normal = (point - vc)^/r
                 return $ HitEvent param point normal mat
