12ad0e2ca24257af186f61f5316af94385620390
Naoki Nishida <domitry@gmail.com>
2020-03-30 12:56:15 +0900

    indent

--- app/Materials.hs
+++ app/Materials.hs
@@ -1,102 +1,101 @@
-module Materials
-    where
-        import Types
-        import Utils
-        import Linear.V3
-        import Linear.Vector
-        import Linear.Metric
-        import Data.Maybe
-        import Data.Vector
-        import System.IO
-        import Control.Monad
+module Materials where
+    import Linear.V3
+    import Linear.Vector
+    import Linear.Metric
+    import Data.Maybe
+    import Data.Vector
+    import System.IO
+    import Control.Monad
+    import Types
+    import Utils
 
-        fromImage::Image->Texture
-        fromImage (Image (w, h) cols) = Texture { pickColor=imp_pick }
-            where
-                vecCols = fromList cols 
-                imp_pick (u, v) _ = case (vecCols!?(x+y*w)) of
-                    Nothing  ->  V3 0.77 0.11 0.54 -- pink if not found
-                    Just col -> col
-                    where
-                        y = floor $ (1-v)*((fromIntegral h)-0.001)
-                        x = floor $ u*(fromIntegral w)
+    fromImage::Image->Texture
+    fromImage (Image (w, h) cols) = Texture { pickColor=imp_pick }
+        where
+            vecCols = fromList cols 
+            imp_pick (u, v) _ = case (vecCols!?(x+y*w)) of
+                Nothing  ->  V3 0.77 0.11 0.54 -- pink if not found
+                Just col -> col
+                where
+                    y = floor $ (1-v)*((fromIntegral h)-0.001)
+                    x = floor $ u*(fromIntegral w)
 
-        fromColor::Color->Texture
-        fromColor albedo = Texture { pickColor=imp_pick }
-            where
-                imp_pick uv xy =  albedo
+    fromColor::Color->Texture
+    fromColor albedo = Texture { pickColor=imp_pick }
+        where
+            imp_pick uv xy =  albedo
 
-        checker::Color->Color->Texture
-        checker col1 col2 = Texture { pickColor=imp_pick }
-            where
-                imp_pick _ (V3 x y z) = if sign>0 then col1 else col2
-                    where
-                        sign = (sin$10*x)*(sin$10*y)*(sin$10*z)
+    checker::Color->Color->Texture
+    checker col1 col2 = Texture { pickColor=imp_pick }
+        where
+            imp_pick _ (V3 x y z) = if sign>0 then col1 else col2
+                where
+                    sign = (sin$10*x)*(sin$10*y)*(sin$10*z)
 
-        -- diffuse
-        lambertian::Texture->Material
-        lambertian tex = Material { scatter=imp_scatter }
-            where
-                imp_scatter _ ev = do
-                    let (n, point, uv) = (evNormal ev, evPoint ev, evUV ev)
-                    v <- randomPointInUnitSphere
-                    let v' = n + v
-                    let col = pickColor tex uv point
-                    return $ (Just $ Ray point v', col)
+    -- diffuse
+    lambertian::Texture->Material
+    lambertian tex = Material { scatter=imp_scatter }
+        where
+            imp_scatter _ ev = do
+                let (n, point, uv) = (evNormal ev, evPoint ev, evUV ev)
+                v <- randomPointInUnitSphere
+                let v' = n + v
+                let col = pickColor tex uv point
+                return $ (Just $ Ray point v', col)
 
-        -- be careful that dot vin nv < 0 and nv is normalized
-        reflect::Vf->Vf->Vf
-        reflect vin n = let vn = abs $ dot vin n in vin + (2*vn)*^n               
+    -- be careful that dot vin nv < 0 and nv is normalized
+    reflect::Vf->Vf->Vf
+    reflect vin n = let vn = abs $ dot vin n in vin + (2*vn)*^n               
 
-        -- be careful that dot vin nv < 0 and nv is normalized
-        refract::Vf->Vf->Float->Maybe Vf
-        refract nvin nv n_over_n'
-            | d > 0 = Just $ (n_over_n')*^hor + (sqrt d)*^vert 
-            | otherwise = Nothing
-            where
-                dt = dot nvin nv
-                hor = nvin - dt*^nv -- horizonal comp of vin (sign is the same as vin)
-                vert = (-1) *^ nv -- vertical comp of vin (sign is the same as vin)
-                d = 1 - (n_over_n' * n_over_n')*(1-dt*dt)
+    -- be careful that dot vin nv < 0 and nv is normalized
+    refract::Vf->Vf->Float->Maybe Vf
+    refract nvin nv n_over_n'
+        | d > 0 = Just $ (n_over_n')*^hor + (sqrt d)*^vert 
+        | otherwise = Nothing
+        where
+            dt = dot nvin nv
+            hor = nvin - dt*^nv -- horizonal comp of vin (sign is the same as vin)
+            vert = (-1) *^ nv -- vertical comp of vin (sign is the same as vin)
+            d = 1 - (n_over_n' * n_over_n')*(1-dt*dt)
 
-        -- metal
-        metal::Color->Float->Material
-        metal albedo fuzziness = Material { scatter=imp_scatter }
-            where
-                imp_scatter (Ray _ vin) ev = do
-                    let (n, point) = (evNormal ev, evPoint ev)
-                    let f = bound (0, 1) fuzziness
-                    s <- randomPointInUnitSphere
-                    let vout = (reflect vin n) + f*^s
-                    return $ if (dot vout n) >= 0
-                        then (Just $ Ray point vout, albedo)
-                        else (Nothing, albedo)
-        
-        -- glass
-        dielectric::Float->Material
-        dielectric ri = Material { scatter=imp_scatter }
-            where
-                schlick cosine ri = r0+(1-r0)*((1-cosine)**5)
-                    where
-                        sqrt_r0 = (1-ri)/(1+ri)
-                        r0 = sqrt_r0*sqrt_r0
+    -- metal
+    metal::Color->Float->Material
+    metal albedo fuzziness = Material { scatter=imp_scatter }
+        where
+            imp_scatter (Ray _ vin) ev = do
+                let (n, point) = (evNormal ev, evPoint ev)
+                let f = bound (0, 1) fuzziness
+                s <- randomPointInUnitSphere
+                let vout = (reflect vin n) + f*^s
+                return $ if (dot vout n) >= 0
+                    then (Just $ Ray point vout, albedo)
+                    else (Nothing, albedo)
+    
+    -- glass
+    dielectric::Float->Material
+    dielectric ri = Material { scatter=imp_scatter }
+        where
+            schlick cosine ri = r0+(1-r0)*((1-cosine)**5)
+                where
+                    sqrt_r0 = (1-ri)/(1+ri)
+                    r0 = sqrt_r0*sqrt_r0
 
-                imp_scatter (Ray _ vin) ev = do
-                    let (n, point) = (evNormal ev, evPoint ev)
-                    let nvin = normalize vin
-                    let nvinn = dot nvin n
-                    
-                    let (normal, n_over_n', cosine) = if nvinn < 0
-                        then (n, 1/ri, -nvinn)
-                        else ((-1)*^n, ri, nvinn) -- the last element is ri*nvinnn in the book
-                    
-                    let reflected = reflect vin normal
+            imp_scatter (Ray _ vin) ev = do
+                let (n, point) = (evNormal ev, evPoint ev)
+                let nvin = normalize vin
+                let nvinn = dot nvin n
+                
+                let (normal, n_over_n', cosine) = if nvinn < 0
+                    then (n, 1/ri, -nvinn)
+                    else ((-1)*^n, ri, nvinn) -- the last element is ri*nvinnn in the book
+                
+                let reflected = reflect vin normal
 
-                    prob <- randomRng (0, 1)
-                    let prob_reflect = schlick cosine n_over_n'
+                prob <- randomRng (0, 1)
+                let prob_reflect = schlick cosine n_over_n'
 
-                    let vout = case (refract nvin normal n_over_n') of
-                            Nothing -> reflected
-                            Just refracted -> if prob < prob_reflect then reflected else refracted
-                    
-                    return (Just $ Ray point vout, V3 1 1 1)
+                let vout = case (refract nvin normal n_over_n') of
+                        Nothing -> reflected
+                        Just refracted -> if prob < prob_reflect then reflected else refracted
+                
+                return (Just $ Ray point vout, V3 1 1 1)
